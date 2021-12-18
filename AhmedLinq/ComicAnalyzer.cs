8ab835ee5f3d41f1e6c9de17d4e8c42a7e67349b
Ahmed Hafez <Ahmed.abdelghany.Hafez@gmail.com>
2021-12-18 07:42:44 +0200

    ComicAnalyzer

---
+++ AhmedLinq/ComicAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AhmedLinq
+{
+    static class ComicAnalyzer
+    {
+        static private PriceRange CalculatePriceRange(Comic comic)
+        {
+           return Comic.Prices[comic.Issue] < 100? PriceRange.Cheap: PriceRange.Expensive;
+        }
+
+        public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(IEnumerable<Comic> comics, IReadOnlyDictionary<int, decimal> prices)
+        {
+            var groups = from comic in comics
+                         orderby prices[comic.Issue]
+                         group comic by CalculatePriceRange(comic) into priceGroup
+                         select priceGroup;
+
+            return groups;
+        }
+
+        public static IEnumerable<string> GetReviews(IEnumerable<Comic> comics, IEnumerable<Review> reviews)
+        {
+            var comicReviews = from comic in comics
+                               orderby comic.Issue
+                               join review in reviews
+                               on comic.Issue equals review.Issue
+                               select $"{review.Critic} rated #{review.Issue} '{comic.Name}' {review.Score:0.00}";
+
+            return comicReviews;
+        }
+    }
+}
---
+++ AhmedLinq/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhmedLinq
+{
+    class Review
+    {
+        public int Issue { get; set; }
+        public Critics Critic { get; set; }
+        public double Score { get; set; }
+    }
+}
