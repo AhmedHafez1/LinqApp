dbe03113830ec7cde01ddd2a6ba3551c8eeb4a52
Ahmed Hafez <Ahmed.abdelghany.Hafez@gmail.com>
2021-12-18 23:11:48 +0200

    LambdaSwitch

---
+++ Lambda/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
+            tallGuy.Honk();
+
+            var array = new[] { 5, 16, 7, 18, 9, 11, 14, 19 };
+            var result1 = from i in array select 2 * i;
+            var result2 = array.Select(i => 2 * i).OrderBy(i => i);
+
+            foreach (var i in result1)
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (var i in result2)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            IEnumerable<int> result3 = from v in values
+                                      where v < 37
+                                      orderby -v
+                                      select v;
+
+            var result4 = values.Where(v => v < 37).OrderByDescending(v => v);
+        }
+    }
+}
---
+++ Lambda/TallGuy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class TallGuy : IClown
+    {
+        public string Name;
+        public int Height;
+        public int Count;
+
+        public string FunnyThingIHave => "big red shoes";
+
+        public void Honk() => Console.WriteLine("Honk honk!");
+
+        public void TalkAboutYourself() => Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+
+        public string ScaryThing => $"{Count} spiders"; 
+    }
+}
